f6873aa1eeb68a59089ce0bf83b8ee2f1e0eb9b4
Renan Medeiros <renanmedeiros79@gmail.com>
2024-07-10 17:57:49 -0300

    Configurando o Router (frontend/src/App.js)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Provider store={store}>
+            <Router>
+                <Switch>
+                    <Route path="/dashboard" component={Dashboard} />
+                    <Route path="/" component={Login} />
+                </Switch>
+            </Router>
+        </Provider>
+    );
 }
 
 export default App;
