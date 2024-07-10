c2464e97cde5ffeb1ed33e3fdc9f1380a49cfa3e
Renan Medeiros <renanmedeiros79@gmail.com>
2024-07-10 17:33:23 -0300

    Configurando Mongoose e Passport (backend/config/keys.js)

---
+++ backend/config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    spotifyClientID: process.env.SPOTIFY_CLIENT_ID,
+    spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    spotifyCallbackURL: process.env.SPOTIFY_CALLBACK_URL,
+    jwtSecret: process.env.JWT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+};
